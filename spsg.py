a7912c831bde096428f4b43c188f3cedcdddc4f6
n00bady <kaz00@vivaldi.net>
2021-09-26 00:03:13 +0300

    Added markdown.extensions.extra to support code blocks, and nestes tags,etc

--- spsg.py
+++ spsg.py
@@ -5,7 +5,7 @@ import markdown
 outfile = 'blog.html'
 # TODO: Add taking input/output files as cli arguments
 with open('test.md', 'r') as md:
-    m = markdown.Markdown(extensions=['markdown.extensions.meta'])
+    m = markdown.Markdown(extensions=['markdown.extensions.meta', 'markdown.extensions.extra'])
     html = m.convert(md.read())
 
 content = jinja2.Environment(loader=jinja2.FileSystemLoader('./')).get_template('template.html').render(body=html, meta=m.Meta)
