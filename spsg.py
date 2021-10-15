0ca31b2e33e22231ddadc67fd0b9ce5545e3c0a4
n00bady <kaz00@vivaldi.net>
2021-10-15 15:25:23 +0300

    Added a simple check to test if each file is a markdown file.

--- spsg.py
+++ spsg.py
@@ -23,24 +23,28 @@ all_posts = []
 
 for file in os.listdir(inpath):
     print("Processing:", file)
-    # TODO: Make it check if they are indeed markdown files.
     md = os.path.join(inpath, file)
-
-    with open(md, 'r') as f:
-        m = markdown.Markdown(extensions=['markdown.extensions.meta', 'markdown.extensions.extra'])
-        html = m.convert(f.read())
     
-    content = jinja2.Environment(loader=jinja2.FileSystemLoader('./')).get_template(template).render(body=html, meta=m.Meta)
-    # There is probably a beter way but I currently I get the title and date from the markdown metadata
-    # and then I create a dictionary called post with them and I also include the path for the file.
-    title = m.Meta.get("title")
-    date = m.Meta.get("date")
-    post = {"title": title[0], "date": date[0], "path": os.path.splitext(file)[0]+'.html'}
-    # I then add them in the list all_posts .
-    all_posts.append(post)
+    # Check if they are indeed markdown files.
+    if file.endswith('.md'):
+        with open(md, 'r') as f:
+            m = markdown.Markdown(extensions=['markdown.extensions.meta', 'markdown.extensions.extra'])
+            html = m.convert(f.read())
+        
+        content = jinja2.Environment(loader=jinja2.FileSystemLoader('./')).get_template(template).render(body=html, meta=m.Meta)
+        # There is probably a beter way but I currently I get the title and date from the markdown metadata
+        # and then I create a dictionary called post with them and I also include the path for the file.
+        title = m.Meta.get("title")
+        date = m.Meta.get("date")
+        post = {"title": title[0], "date": date[0], "path": os.path.splitext(file)[0]+'.html'}
+        # I then add them in the list all_posts .
+        all_posts.append(post)
 
-    outfile = os.path.join(outpath, os.path.splitext(file)[0]+'.html')
-    with open(outfile, 'w') as o: o.write(content)
+        outfile = os.path.join(outpath, os.path.splitext(file)[0]+'.html')
+        with open(outfile, 'w') as o: o.write(content)
+    else:
+        print(file, "is not a markdown file!")
+        print("\tIt will be skiped!")
 
 # Here is where the index page is constructed.
 print("\n*** Generating index page ***")
